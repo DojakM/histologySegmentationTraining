73ee06cf74a2b3abca45544ddf2a8ef5c32bc19b
dominikmolitor <dominik-jakob.molitor@protonmail.com>
2023-03-23 22:42:31 +0100

    STN going

--- histology_segmentation_training/models/model_components.py
+++ histology_segmentation_training/models/model_components.py
@@ -58,12 +58,10 @@ class UnetUp(nn.Module):
             outputs0 = torch.cat([outputs0, feature], dim=1)
         return self.conv(outputs0)
 
-
-class UnetSPT(nn.Module):
-    def __init__(self, in_size, out_size, dim_h, stride=1, ks=3, dropout_val=0, gpus=False):
-        super(UnetSPT, self).__init__()
-        self.dim_h = dim_h
-        self.ought = int(np.floor((np.floor((dim_h - 6) / 2) - 4) / 2))
+#### ==== Spatial Transformer kinda doesn't do anything ==== ####
+class SPTTo(nn.Module):
+    def __init__(self, in_size, out_size, stride=1, ks=3, dropout_val=0, gpus=False):
+        super(SPTTo, self).__init__()
 
         self.conv = nn.Sequential(nn.Sequential(
             nn.Dropout(dropout_val),
@@ -77,23 +75,20 @@ class UnetSPT(nn.Module):
 
         # spatial transformer localization network
         self.localization = nn.Sequential(
-            nn.Conv2d(in_size, 8, kernel_size=7),  # 256*256*3
+            nn.Conv2d(3, 32, kernel_size=7),  # 256*256*3
             nn.MaxPool2d(2, stride=2),  # 250*250*8
             nn.ReLU(True),
-            nn.Conv2d(8, 16, kernel_size=5),  # 125*125*8
+            nn.Conv2d(32, 64, kernel_size=5),  # 125*125*8
             nn.MaxPool2d(2, stride=2),  # 121*121*16
             nn.ReLU(True)  # 60*60*16
         )
         # tranformation regressor for theta
         self.fc_loc = nn.Sequential(
-            nn.Linear((self.ought ** 2) * 16, 32),
-            nn.ReLU(True),
-            nn.Linear(32, 3 * 2)
+            nn.Linear(1024, 16),
+            nn.Linear(16, 3 * 2)
         )
-        # initializing the weights and biases with identity transformations
-        self.fc_loc[2].weight.data.zero_()
-        self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0],
-                                                    dtype=torch.float))
+        self.fc_loc[1].weight.data.zero_()
+        self.fc_loc[1].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
 
     def stn(self, x):
         xs = self.localization(x)
@@ -107,25 +102,42 @@ class UnetSPT(nn.Module):
     def rev_stn(self, x, theta: torch.tensor):
         empty_tensor = torch.empty(theta.size())
         for val, batch in enumerate(theta):
-            new_theta = torch.cat((batch, torch.tensor([[0,0,1]])), 0)
+            new_theta = torch.cat((batch, torch.tensor([[0, 0, 1]])), 0)
             theta = new_theta.inverse()
-            theta = theta[:2,:]
+            theta = theta[:2, :]
             empty_tensor[val, :, :] = theta
         grid = F.affine_grid(empty_tensor, x.size())
         x = F.grid_sample(x, grid)
         return x
 
     def forward(self, x):
-        # transform the input
-
         x, theta = self.stn(x)
         x = self.conv(x)
-        x = self.rev_stn(x, theta)
+        return x, theta
 
+class SPTBack(nn.Module):
+    def __init__(self, in_size, out_size):
+        super(SPTBack, self).__init__()
+        self.conv = nn.Conv2d(in_size, out_size, 1)
+
+    def rev_stn(self, x, theta: torch.tensor):
+        empty_tensor = torch.empty(theta.size())
+        for val, batch in enumerate(theta):
+            new_theta = torch.cat((batch, torch.tensor([[0, 0, 1]])), 0)
+            theta = new_theta.inverse()
+            theta = theta[:2, :]
+            empty_tensor[val, :, :] = theta
+        grid = F.affine_grid(empty_tensor, x.size())
+        x = F.grid_sample(x, grid)
         return x
 
-#### ==== In Testing ==== ####
+    def forward(self, x, theta):
+        x = self.conv(x)
+        self.rev_stn(x, theta)
+        return x
 
+
+#### ==== In Testing ==== ####
 class Context(nn.Module):
     def __init__(self, in_size, out_size, gpus=False):
         super(Context, self).__init__()
--- histology_segmentation_training/models/unet_instance.py
+++ histology_segmentation_training/models/unet_instance.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from models.model_components import UnetConv, UnetUp, UnetSPT, Context, Localization
+from models.model_components import UnetConv, UnetUp, SPTTo, SPTBack, Context, Localization
 from models.unet_super import UnetSuper
 from utils import weights_init
 
@@ -69,17 +69,19 @@ class RTUnet(UnetSuper):
             self.is_deconv = is_deconv
             self.is_batchnorm = is_batchnorm
             self.input = input_channels
-            filters = [32, 64, 128, 256]
-            self.conv1 = UnetSPT(self.in_channels, filters[0], 256, gpus=on_gpu, dropout_val=kwargs["dropout_val"])
-            self.conv2 = UnetSPT(filters[0], filters[1], 128,  gpus=on_gpu, dropout_val=kwargs["dropout_val"])
-            self.conv3 = UnetSPT(filters[1], filters[2], 64,  gpus=on_gpu, dropout_val=kwargs["dropout_val"])
-            self.center = UnetSPT(filters[2], filters[3], 32, gpus=on_gpu, dropout_val=kwargs["dropout_val"])
+            filters = [8, 16, 32, 64]
+
+            self.conv1 = SPTTo(self.in_channels, filters[0], gpus=on_gpu, dropout_val=kwargs["dropout_val"])
+            self.conv2 = UnetConv(filters[0], filters[1], True, gpus=on_gpu, dropout_val=kwargs["dropout_val"])
+            #self.conv3 = UnetConv(filters[1], filters[2], True, gpus=on_gpu, dropout_val=kwargs["dropout_val"])
+            self.center = UnetConv(filters[1], filters[2], True, gpus=on_gpu, dropout_val=kwargs["dropout_val"])
             # upsampling
             self.up_concat3 = UnetUp(filters[3], filters[2], True, gpus=on_gpu, dropout_val=kwargs["dropout_val"])
             self.up_concat2 = UnetUp(filters[2], filters[1], True, gpus=on_gpu, dropout_val=kwargs["dropout_val"])
             self.up_concat1 = UnetUp(filters[1], filters[0], True, gpus=on_gpu, dropout_val=kwargs["dropout_val"])
+
             # final conv (without any concat)
-            self.final = nn.Conv2d(filters[0], kwargs["num_classes"], 1)
+            self.final = SPTBack(filters[0], 7)
             if on_gpu:
                 self.conv1.cuda()
                 self.conv2.cuda()
@@ -91,26 +93,35 @@ class RTUnet(UnetSuper):
                 self.final.cuda()
             self.apply(weights_init)
 
-        def forward(self, inputs):
+        def forward(self, inputs: torch.Tensor):
             maxpool = nn.MaxPool2d(kernel_size=2)
-            conv1 = self.conv1(inputs)  # 16*256*256
-            maxpool1 = maxpool(conv1)  # 16*128*128
+            x_s = torch.chunk(inputs, 8, 2)
+            along_x = []
+            for i in x_s:
+                chunks = torch.chunk(i, 8, 3)
+                along_x.append(chunks)
+            merge_x = []
+            for chunks in along_x:
+                merge_y = []
+                for chunk in chunks:
+                    conv1, theta = self.conv1(chunk)  # 16*64*64
+                    maxpool1 = maxpool(conv1)  # 16*32*32
 
-            conv2 = self.conv2(maxpool1)  # 32*128*128
-            maxpool2 = maxpool(conv2)  # 32*64*64
+                    conv2 = self.conv2(maxpool1)  # 32*32*32
+                    maxpool2 = maxpool(conv2)  # 32*16*16
 
-            conv3 = self.conv3(maxpool2)  # 64*64*64
-            maxpool3 = maxpool(conv3)  # 64*32*32
+                    center = self.center(maxpool2)
 
-            center = self.center(maxpool3)
+                   # up3 = self.up_concat3(center, conv2)  # 64*16*16
+                    up2 = self.up_concat2(center, conv2)  # 32*32*32
+                    up1 = self.up_concat1(up2, conv1)  # 16*64*64
 
-            up3 = self.up_concat3(center, conv3)  # 64*64*64
-            up2 = self.up_concat2(up3, conv2)  # 32*128*128
-            up1 = self.up_concat1(up2, conv1)  # 16*256*256
+                    final = self.final(up1, theta)
+                    finalize = nn.functional.softmax(final, dim=1)
+                    merge_y.append(finalize)
+                merge_x.append(torch.cat(merge_y, 3))
+            return torch.cat(merge_x, 2)
 
-            final = self.final(up1)
-            finalize = nn.functional.softmax(final, dim=1)
-            return finalize
 
 
 #### ==== Context Unet - just testing ==== ####
